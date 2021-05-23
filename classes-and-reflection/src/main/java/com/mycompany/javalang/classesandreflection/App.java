744ab1a976f10d44a72c7bbaf18a88f30ad7126f
Alex <babkin.alal@gmail.com>
2021-05-24 01:05:53 +0300

    added reflection

--- classes-and-reflection/src/main/java/com/mycompany/javalang/classesandreflection/App.java
+++ classes-and-reflection/src/main/java/com/mycompany/javalang/classesandreflection/App.java
@@ -1,14 +1,15 @@
 package com.mycompany.javalang.classesandreflection;
 
 import com.mycompany.javalang.classesandreflection.point.Point;
+import com.mycompany.javalang.classesandreflection.reflection.Stringifier;
 import com.mycompany.javalang.classesandreflection.shape.Line;
 
 public class App {
     public static void main(String[] args) {
         Line line = new Line(new Point(1, 1), new Point(2, 2));
         try {
-            line.clone();
-        } catch (CloneNotSupportedException ex) {
+            System.out.println(Stringifier.toString(line));
+        } catch (IllegalAccessException ex) {
             System.out.println(ex);
         }
     }
